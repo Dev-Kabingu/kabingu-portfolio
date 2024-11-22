a47509a229a5ac1295718a5072987fb4becde4a0
dev-kabingu <missmercykabingu@gmail.com>
2024-11-22 14:25:10 +0300

    added more features

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("navList");
+
+hamburger.addEventListener("click", function(){
+    navList.classList.toggle("active");
+});
